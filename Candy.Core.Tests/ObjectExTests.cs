9244e88a03d0dee913d2a930a654015edf2c3103
johnsmith1st <johnsmith1st@live.com>
2016-10-14 22:49:07 +0800

    add object as

---
+++ Candy.Core.Tests/ObjectExTests.cs
@@ -0,0 +1,20 @@
+﻿using Candy.System;
+using Xunit;
+
+namespace Tests
+{
+    public class ObjectExTests
+    {
+        [Fact]
+        public void TestAs() 
+        {
+            var x = new Foo();
+            Assert.NotNull(x.As<IFoo>());
+            Assert.Null(x.As<IBar>());
+        }
+
+        public interface IFoo { }
+        public interface IBar { }
+        public class Foo : IFoo { }
+    }
+}
---
+++ Candy.Core/ObjectEx.cs
@@ -0,0 +1,7 @@
+﻿namespace Candy.System
+{
+    public static class ObjectEx
+    {
+        public static T As<T>(this object x) => x is T ? (T)x : default(T);
+    }
+}
