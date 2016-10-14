2031d8fa306616e383798a9a340816aa9ac19b2f
johnsmith1st <johnsmith1st@live.com>
2016-10-15 00:18:53 +0800

    refine fore each, return void

--- Candy.Core.Tests/CollectionExTests.cs
+++ Candy.Core.Tests/CollectionExTests.cs
@@ -8,12 +8,8 @@ namespace Candy.Core.Tests
         public void TestForEach()
         {
             int i = 0, j = 0;
-
-            Utils
-                .Range(0, 5)
-                .ForEach(x => i += x)
-                .ForEach((x, y) => j += (x * y));
-
+            Utils.Range(0, 5).ForEach(x => i += x);
+            Utils.Range(0, 5).ForEach((x, y) => j += (x * y));
             Assert.Equal(10, i);
             Assert.Equal(30, j);
         }
--- Candy.Core/CollectionEx.cs
+++ Candy.Core/CollectionEx.cs
@@ -5,17 +5,15 @@ namespace Candy
 {
     public static class CollectionEx
     {
-        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> handler)
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> handler)
         {
             foreach (var item in source) handler.Invoke(item);
-            return source;
         }
 
-        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T, int> handler)
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> handler)
         {
             var i = 0;
             foreach (var item in source) handler.Invoke(item, i++);
-            return source;
         }
     }
 }
